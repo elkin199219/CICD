c1fb1d3390b181cef18ffb323e5c2b94e8517387
elkin199219 <elkin_620@hotmail.com>
2024-10-11 20:05:42 -0500

    Update HttpRequestTest.java

--- src/test/java/co/edu/eafit/protobootapp/HttpRequestTest.java
+++ src/test/java/co/edu/eafit/protobootapp/HttpRequestTest.java
@@ -10,7 +10,7 @@ import org.springframework.boot.web.server.LocalServerPort;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//Pruebas de integracion
+//Pruebas de integracion.
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HttpRequestTest {
 
